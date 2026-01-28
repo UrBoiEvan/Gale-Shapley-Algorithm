f6d0eb6e1ed972c6dbc526a0d459261f77592fe4
Evan Harden <evan145447@gmail.com>
2026-01-28 18:00:41 -0500

    Added stable matching algorithm

--- src/main.py
+++ src/main.py
@@ -1,12 +1,14 @@
+from collections import defaultdict
 def parseFile(file):
     targetFile = open(file,"r")
     line = targetFile.readline()
     n = int(line.strip())
     hospital_ranks, student_ranks = {}, {}
-    i = 0
-    while line and i < 2*n:
+    i = 1
+    while line and i <= 2*n:
         line = targetFile.readline().split()
-        if i < n:
+        line = [int(char) for char in line]
+        if i < n+1:
             hospital_ranks[i] = list(line)
         else:
             student_ranks[i-n] = list(line)
@@ -16,13 +18,32 @@ def parseFile(file):
 
 def stableMatching(hospital_ranks,student_ranks):
     # init all hospitals to free
-    free_hospitals = list(hospitals.keys())
-    print(free_hospitals)
-    # TODO implement algorithm
-    return
+    free_hospitals = list(hospital_ranks.keys())
+    proposal_idx = defaultdict(int) # track which student a particular hospital is iterating with the list
+    student_assignments = {} # {Student: Hospital}
+    while len(free_hospitals) > 0:
+        h = free_hospitals[0] # choose front of queue
+        a = hospital_ranks[h][proposal_idx[h]] # student next for proposal
+        if a not in student_assignments: # student is free
+            student_assignments[a] = h
+            free_hospitals.pop(0)
+        elif student_ranks[a].index(h) < student_ranks[a].index(student_assignments[a]): # student may trade up
+            # dump current match
+            h_old = student_assignments[a]
+            free_hospitals.append(student_assignments[a])
+            proposal_idx[h_old] += 1 # move old hospital proposal to the next entry
+            # new hospital
+            student_assignments[a] = h
+            free_hospitals.pop(0)
+        else: # a rejects h, increment h proposal for next student
+            proposal_idx[h] += 1
+    return {h: a for a, h in student_assignments.items()} # flip to {Hospital: Student}
+
+def main():
+    hospital_ranks, student_ranks = parseFile("example.in")
+    res = stableMatching(hospital_ranks, student_ranks)
+    print("Hospital Ranks: ",res)
+    #TODO verify stable match
 
 if __name__=="__main__":
-    hospital_ranks, student_ranks = parseFile("example.in")
-    print(hospital_ranks)
-    print(student_ranks)
-    stableMatching(hospital_ranks,student_ranks)
+    main()
