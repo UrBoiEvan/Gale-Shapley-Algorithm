0a049d11cddb0f8446d1215abe47a17dadecb722
Semyon Baykov <simon.baykov@gmail.com>
2026-01-29 18:18:46 -0500

    Fix task A: increment i on proposal, pop hospital on reject, fix bounds edge case

--- src/main.py
+++ src/main.py
@@ -26,22 +26,26 @@ def stableMatching(hospital_ranks,student_ranks):
     free_hospitals = list(hospital_ranks.keys())
     proposal_idx = defaultdict(int) # track which student a particular hospital is iterating with the list
     student_assignments = {} # {Student: Hospital}
+
     while len(free_hospitals) > 0:
         h = free_hospitals[0] # choose front of queue
+        if proposal_idx[h] >= len(hospital_ranks[h]):   # If hospital exhausted its list, remove it
+            free_hospitals.pop(0)
+            continue
+
         a = hospital_ranks[h][proposal_idx[h]] # student next for proposal
-        if a not in student_assignments: # student is free
+        proposal_idx[h] += 1  # advance proposal pointer
+
+        if a not in student_assignments:    # student is free
             student_assignments[a] = h
             free_hospitals.pop(0)
-        elif student_ranks[a].index(h) < student_ranks[a].index(student_assignments[a]): # student may trade up
-            # dump current match
+        else:
             h_old = student_assignments[a]
-            free_hospitals.append(student_assignments[a])
-            proposal_idx[h_old] += 1 # move old hospital proposal to the next entry
-            # new hospital
-            student_assignments[a] = h
-            free_hospitals.pop(0)
-        else: # a rejects h, increment h proposal for next student
-            proposal_idx[h] += 1
+            if student_ranks[a].index(h) < student_ranks[a].index(h_old):   # else: student rejects h next i, hospital stays free
+                student_assignments[a] = h
+                free_hospitals.pop(0)
+                free_hospitals.append(h_old)
+
     return {h: a for a, h in student_assignments.items()} # flip to {Hospital: Student}
 
 def main():
